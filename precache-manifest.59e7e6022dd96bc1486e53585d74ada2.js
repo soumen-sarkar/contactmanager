46b295e7a344363d3188cd6b7c8c126f49213ac1
Soumen Sarkar <soumen.rim@gmail.com>
2020-04-25 10:22:02 +0530

    Updates

---
+++ precache-manifest.59e7e6022dd96bc1486e53585d74ada2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bc873cbbc3871b4b592519321b4336d8",
+    "url": "/contactmanager/index.html"
+  },
+  {
+    "revision": "4e8a8f8f2cddfa52f307",
+    "url": "/contactmanager/static/css/2.69121389.chunk.css"
+  },
+  {
+    "revision": "0a7b97b64e7c33613292",
+    "url": "/contactmanager/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "4e8a8f8f2cddfa52f307",
+    "url": "/contactmanager/static/js/2.72ff10d1.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/contactmanager/static/js/2.72ff10d1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0a7b97b64e7c33613292",
+    "url": "/contactmanager/static/js/main.efdebeb7.chunk.js"
+  },
+  {
+    "revision": "98d49c782551fbed3aca",
+    "url": "/contactmanager/static/js/runtime-main.53501af3.js"
+  }
+]);
